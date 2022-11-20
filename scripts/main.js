bad28dccdb57f38563a4d05b747e678ad8a2f196
Rene <84898888+rene-xfxt@users.noreply.github.com>
2022-11-20 09:02:14 +0800

    2022.11.20morning

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+myyImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
       myImage.setAttribute('src', 'images/firefox2.png');
